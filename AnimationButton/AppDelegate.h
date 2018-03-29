7075a637d95e92c560243fbfd55124875d2a5573
mars <627556038@qq.com>
2018-03-29 10:03:50 +0800

    Initial Commit

---
+++ AnimationButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationButton
+//
+//  Created by 郭海祥 on 2018/3/29.
+//  Copyright © 2018年 ghx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationButton
+//
+//  Created by 郭海祥 on 2018/3/29.
+//  Copyright © 2018年 ghx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
